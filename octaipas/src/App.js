090048477d2a24f8a5182650e6e8f60e9d0ee866
pontus <pontus.g91@gmail.com>
2019-04-24 10:37:08 +0200

    tested adding a react component, tested send state to created component and print props

--- octaipas/src/App.js
+++ octaipas/src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
+import Divcom from './components/divcom';
+
 
 class App extends Component {
+  state = {
+  drinks: [
+    {
+    id: "1",
+    name: "vodka"
+  },
+  {
+    id: "2",
+    name: "burbon"
+  }
+]
+}
+
   render() {
+    console.log(this.state.drinks);
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            hej
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Divcom drinks={this.state.drinks}/>
       </div>
     );
   }
